b37e067b0c101d7fec42972c13400464e22c36b1
Yinhe Zhu <zhuyinhe@hotmail.com>
2026-03-06 08:18:13 +0200

    completed exercise 4.4: Helper Function and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (array) => {
+    const reducer = (total, blog) => {
+        return total + (blog.likes || 0)
+    }
+    return array.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
